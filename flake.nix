15312850275c44fd98c381516ea8f4db7f345de2
Alexandre Hallaine <alexandre@hallaine.com>
2025-08-14 21:33:43 +0000

    feat: nix flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+          ];
+        };
+      }
+    );
+}
